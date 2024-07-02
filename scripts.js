b413c5921e7c27f36b54f3a5a5c7598debd9da21
Kayvon Kazemini <kayvonk@gmail.com>
2024-07-02 16:18:50 -0600

    initial commit

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
-function shuffleCards() {}
-
 let startBtn = document.getElementById("startBtn");
+let catImg = document.querySelector(".catImg");
+
+let correctCard = "card2";
 
 startBtn.addEventListener("click", function () {
   startBtn.style.display = "none";
@@ -9,7 +10,28 @@ startBtn.addEventListener("click", function () {
 
   for (let i = 0; i < cards.length; i++) {
     const element = cards[i];
-
     element.classList.add("flip");
+    catImg.classList.add("reveal");
+    setTimeout(() => {
+      element.classList.remove("flip");
+      catImg.classList.remove("reveal");
+    }, 4000);
   }
+});
+
+let cards = document.querySelectorAll(".card");
+
+cards.forEach((card) => {
+  card.addEventListener("click", function (e) {
+    let element = e.target;
+    let cardNumber = element.classList[1];
+    if (cardNumber === correctCard) {
+      element.style.animationIterationCount = "0.5";
+      catImg.style.animationIterationCount = "0.5";
+
+      catImg.classList.add("reveal");
+      console.log("Winner!");
+    }
+    element.classList.add("flip");
+  });
 });
